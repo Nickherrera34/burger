5dd912dc292fdc77c3400a829ab05341f52b551b
Nick Herrera <nickherrera34@gmail.com>
2018-01-09 18:17:50 -0600

    added code to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection;
 
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password:"barton34",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password:"barton34",
+      database: "burgers_db"
+    });
+};
 
-});
 
+  
 connection.connect(function(err){
   if(err){
     console.error("Error connecting to database: " + err.stack);
